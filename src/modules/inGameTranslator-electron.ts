3f5976bd928f95466e85ea674bfe6909cf1ad918
渣渣120 <WOSHIZHAZHA120@qq.com>
2026-03-29 15:21:32 +0800

    :bug: 重写特定文件而不是所有 `res`

--- src/modules/inGameTranslator-electron.ts
+++ src/modules/inGameTranslator-electron.ts
@@ -6,31 +6,31 @@ import { processPlacement } from "@/utils/replacement";
 export default async () => {
     const pluginStateStorageKey = "tetrio_plus_plus_customize-chinese_translate-state";
 
-    createRewriteFilter("tetr.io chinese font replacer", "https://tetr.io/res/font/*", {
+    createRewriteFilter("tetr.io chinese font replacer", "https://tetr.io/res/font/hun.fnt", {
         enabledFor: async (storage) => {
             let res = await storage.get(pluginStateStorageKey);
             return res[pluginStateStorageKey];
         },
         onStop: async (storage, url, src, callback) => {
-            if (url.endsWith("hun.fnt")) {
-                callback({
-                    type: "font/fnt",
-                    data: await processPlacement(replacements["res/font/hun.fnt"], [{ src, storage }]),
-                    encoding: "base64-data-url",
-                });
+            callback({
+                type: "font/fnt",
+                data: await processPlacement(replacements["res/font/hun.fnt"], [{ src, storage }]),
+                encoding: "base64-data-url",
+            });
+        },
+    });
 
-                return;
-            }
-
-            if (url.endsWith("hun.png")) {
-                callback({
-                    type: "image/png",
-                    data: await processPlacement(replacements["res/font/hun.png"], [{ src, storage }]),
-                    encoding: "base64-data-url",
-                });
-
-                return;
-            }
+    createRewriteFilter("tetr.io chinese font replacer", "https://tetr.io/res/font/hun.png", {
+        enabledFor: async (storage) => {
+            let res = await storage.get(pluginStateStorageKey);
+            return res[pluginStateStorageKey];
+        },
+        onStop: async (storage, url, src, callback) => {
+            callback({
+                type: "image/png",
+                data: await processPlacement(replacements["res/font/hun.png"], [{ src, storage }]),
+                encoding: "base64-data-url",
+            });
         },
     });
 
